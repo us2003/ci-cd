f2d30c941da23c98dc4b5935525c7f531293e57c
us2003 <myemail@mysite.xyz>
2022-08-29 16:50:42 +0300

    add counter

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import { useState } from "react";
+
 function App() {
+  const [value, setValue] = useState( 0 );
+  const inc = () => setValue( x => x + 1 );
   return (
     <div className="App">
-      CI-CD
+      <h1>Value: {value}</h1>
+      <button onClick={inc}>Increment</button>
     </div>
   );
 }
